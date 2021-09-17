5f01a26fdabea1e92bf00ce951992fa041b25fb5
chaitanya4294 <chaitanya.4294@gmail.com>
2021-09-17 15:19:17 +0530

    [Chaitanya] Add: Created folder structure and added welcome message

---
+++ LineLengths/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineLengths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\nWelcome to Line lengths comparision problem.");
+        }
+    }
+}
