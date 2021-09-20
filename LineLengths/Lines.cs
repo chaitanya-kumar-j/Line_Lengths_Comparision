7795e174376d7fa73c7a1926134bab6fd8dbb92a
chaitanya4294 <chaitanya.4294@gmail.com>
2021-09-20 08:10:33 +0530

    [Chaitanya] Add: Added line lengths equality checking feature

--- LineLengths/Lines.cs
+++ LineLengths/Lines.cs
@@ -6,11 +6,13 @@ namespace LineLengths
 {
     class Lines
     {
-        public void IsEqual(double l1x1, double l1x2, double l1y1, double l1y2,
-                              double l2x1, double l2x2, double l2y1, double l2y2)
+        public double Length(double x1, double x2, double y1, double y2)
         {
-            double line1Length = Math.Sqrt(Math.Pow(l1x2 - l1x1, 2) + Math.Pow(l1y2 - l1y1, 2));
-            double line2Length = Math.Sqrt(Math.Pow(l2x2 - l2x1, 2) + Math.Pow(l2y2 - l2y1, 2));
+            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            return length;
+        }
+        public void IsEqual(double line1Length, double line2Length)
+        {
             if (line1Length.Equals(line2Length))
                 Console.WriteLine("Both lines are of equal length.");
             else
--- LineLengths/Program.cs
+++ LineLengths/Program.cs
@@ -6,8 +6,12 @@ namespace LineLengths
     {
         static void Main(string[] args)
         {
+            // Two line lengths are...
+            double line1Length = new Lines().Length(0, 6, 0, 8);
+            double line2Length = new Lines().Length(0, 8, 0, 8);
+
             // Check equality of lengths of given two lines.
-            new Lines().IsEqual(0,6,8,0,0,8,6,0);
+            new Lines().IsEqual(line1Length, line2Length);
         }
     }
 }
